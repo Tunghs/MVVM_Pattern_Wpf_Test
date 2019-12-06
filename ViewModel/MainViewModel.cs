9c8d0838080e866634d0d538bf297f6e36624ee3
이성현 <dljdgus1211@naver.com>
2019-12-06 13:02:29 +0900

    Complete Project

--- ViewModel/MainViewModel.cs
+++ ViewModel/MainViewModel.cs
@@ -9,22 +9,34 @@ using System.Windows.Input;
 
 namespace MVVM_test.ViewModel
 {
-    public class MainViewModel:INotifyPropertyChanged
+    public class MainViewModel : INotifyPropertyChanged
     {
         private int _iNum;
 
         // Number에 대하 get, set
         public int Number
         {
-            get 
+            get
             { return _iNum; }
-            set 
-            { 
+            set
+            {
                 _iNum = value;
                 // 이건 set 부분이 호출되어 Number의 값이 바뀌게 되면 UI에게 '나 변했어' 라고 알려 달라는 뜻이다.
                 // 이걸 알려주면 UI는 개발자가 특별한 작업을 하지 않아도 값을 자동으로 바뀐 값으로 변경하게 된다.
                 OnPropertyChanged("Number");
+
+                // Enable이 변경되었음을 알려주어야 한다.
+                OnPropertyChanged("MinusEnable");
+                OnPropertyChanged("PlusEnable");
+
+                PageContents = string.Format("{0} 페이지를 보고 있어요.", _iNum);
             }
+        }
+
+        // 프로그램이 실행되면 1페이지 부터 시작하기 위해서 생성자를 하나 만들고 그 생성자에서 Number=1 을 설정한다.
+        public MainViewModel()
+        {
+            Number = 1;
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
@@ -51,6 +63,12 @@ namespace MVVM_test.ViewModel
             Number--;
         }
 
+        // Number가 1보다 작으면 버튼 비활성화
+        public bool MinusEnable
+        {
+            get { return Number > 1 ? true : false; }
+        }
+
         private ICommand plusCommand;
         public ICommand PlusCommand
         {
@@ -60,6 +78,23 @@ namespace MVVM_test.ViewModel
         {
             Number++;
             
+        }
+
+        // Number가 10이 넘어가면 버튼 비활성화
+        public bool PlusEnable
+        {
+            get { return Number < 10 ? true : false; }
+        }
+
+        private string pageContent;
+        public string PageContents
+        {
+            get { return pageContent; }
+            set
+            {
+                pageContent = value;
+                OnPropertyChanged("PageContents");
+            }
         }
     }
 }
